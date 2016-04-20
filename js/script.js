46c23809aee4b4981dbea644319de967d9713edb
Rafa <alra.alderete@gmail.com>
2016-04-20 04:47:18 -0300

    Estructura HTML/CSS, Jquery, SnapSVG

---
+++ js/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+  var SLIDEWIDTH = 480;
+  var SLIDETIME = 5000;
+  var imageposition = 1;
+  var sliding = false;
+
+  function slideImage (clicked, position) {
+    sliding = true;
+    if (!clicked)
+      if (imageposition == 4)
+        position = -3;
+    var marginvalue = parseInt($(".carousel .slides").css("margin-left")) + (SLIDEWIDTH * -position);
+    $(".carousel .slides").animate({"margin-left":marginvalue},1000,function(){
+        imageposition = imageposition + position;
+        $(".dots .dot").removeClass("active");
+        $("#dot"+String(imageposition)).addClass("active");
+        sliding = false;
+      })
+  }
+
+  var slideTimer = setInterval(slideImage, SLIDETIME, false, 1);
+
+  $('.dots .dot').click(function() {
+    if ((imageposition != $(this).attr('position')) && !(sliding)) {
+      clearInterval(slideTimer);
+      slideImage(true, $(this).attr('position') - imageposition);
+      slideTimer = setInterval(slideImage, SLIDETIME, false, 1);
+    }
+	});
+
+  $('.dropdown').hover(
+       function(){
+           $(this).children('.dropdown-content').stop().slideDown('fast');
+       },
+       function(){
+           $(this).children('.dropdown-content').slideUp('fast');
+       }
+   );
+
+   Snap.select('.svg-facebook').hover(
+     function() {
+       Snap.select('#facebook-background').animate({'fill':'rgb(60, 60, 153)'}, 0.5);
+       Snap.select('#facebook').animate({'fill':'white'}, 0.5);
+     },
+     function() {
+       Snap.select('#facebook-background').animate({'fill':'white'}, 0.5);
+       Snap.select('#facebook').animate({'fill':'black'}, 0.5);
+   });
+
+  $(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 150) {
+        $('nav').addClass('fixed');
+    } else {
+        $('nav').removeClass('fixed');
+    }
+  });
+
+});
